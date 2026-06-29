a3cbc30ed334bb32d7baa08b96855de280ebaa15
Aashish Saini <aashishsaini7337@gmail.com>
2026-06-29 11:05:26 +0530

    update folder

---
+++ api-project/loginsystem/myproject/accounts/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from .views import *
+
+urlpatterns = [
+
+    path('login/', login_view, name='login'),
+
+    path('dashboard/', dashboard, name='dashboard'),
+
+    path('logout/', logout_view, name='logout'),
+
+]
